33bb9da560a0cda3e8beaaee99adba05ac41c18a
TheDefterus <TheDefterus@gmail.com>
2021-10-27 16:33:35 +0200

    commit of base FAST portions, WIP

--- FAST.py
+++ FAST.py
@@ -26,15 +26,16 @@ def fast_test_preliminary(pixels, threshold):
     return it_is_brighter or it_is_darker
 
 
+# apply the quicktest to every pixel in the input image with threshold, output candidates
 def fast_check_every_preliminary(monoc_image, threshold):
     candidates = []
-    for i in range(monoc_image.shape[0]-6):
-        for j in range(monoc_image.shape[1]-6):
-            pixels = [monoc_image[i+3, j+3],
-                      monoc_image[i+3, j+6],
-                      monoc_image[i+6, j+3],
-                      monoc_image[i, j+3],
-                      monoc_image[i+3, j]]
+    for i in range(monoc_image.shape[0] - 6):
+        for j in range(monoc_image.shape[1] - 6):
+            pixels = [monoc_image[i + 3, j + 3],
+                      monoc_image[i + 3, j + 6],
+                      monoc_image[i + 6, j + 3],
+                      monoc_image[i, j + 3],
+                      monoc_image[i + 3, j]]
             if fast_test_preliminary(pixels, threshold):
                 candidates.append([j, i])
     return candidates
@@ -61,19 +62,41 @@ def fast_get_conti_pixels(monoc_image, candidate):
     return values
 
 
+# takes as input the pixel values of the circle (+ center), threshold and outputs boolean regarding candidacy
+def fast_check_continous_9(center_value, pixel_values, threshold):
+    list_of_pixel_values = pixel_values
+    bright_or_dark = []
+    judgement = False
+    for i in range(len(list_of_pixel_values)):
+        temp_value = list_of_pixel_values[i]
+        if temp_value >= center_value + threshold:
+            bright_or_dark += 'b'
+        elif temp_value <= center_value - threshold:
+            bright_or_dark += 'd'
+        else:
+            bright_or_dark += 's'
+    for i in range(len(list_of_pixel_values)):
+        for bds in bright_or_dark:
+            if list_of_pixel_values[0:9] == 'b' or list_of_pixel_values[0:9] == 'b':
+                judgement = True
+                break
+            bright_or_dark.append(bright_or_dark.pop(0))
+    return judgement
+
+
 def fast_end_to_end(monoc_image, threshold):
     cadidate_locations = fast_check_every_preliminary(monoc_image, threshold)
     features_detected_end = []
     return features_detected_end
 
 
-#pic = cv.imread('/home/thekinga/University/Polybot/TestMap_cropped.jpg')
-pic = cv.imread('/home/thekinga/University/PycharmProjects/ROB7ARPproject/aau-city-1.jpg')
-monochrome = cv.cvtColor(pic, cv.COLOR_BGR2GRAY)
-candidate_locations = fast_check_every_preliminary(monochrome, 25)
-for i in range(len(candidate_locations)):
-    cv.circle(pic, candidate_locations[i], 3, (0), 1)
-#features_detected_after_fast = fast_end_to_end(monochrome, 25)
-cv.namedWindow('test')
-cv.imshow('test', pic)
-cv.waitKey()
+# pic = cv.imread('/home/thekinga/University/Polybot/TestMap_cropped.jpg')
+# pic = cv.imread('/home/thekinga/University/PycharmProjects/ROB7ARPproject/aau-city-1.jpg')
+# monochrome = cv.cvtColor(pic, cv.COLOR_BGR2GRAY)
+# candidate_locations = fast_check_every_preliminary(monochrome, 25)
+# for i in range(len(candidate_locations)):
+#     cv.circle(pic, candidate_locations[i], 3, (0), 1)
+# # features_detected_after_fast = fast_end_to_end(monochrome, 25)
+# cv.namedWindow('test')
+# cv.imshow('test', pic)
+# cv.waitKey()
--- FAST_test.py
+++ FAST_test.py
@@ -11,6 +11,18 @@ class MyTestCase(unittest.TestCase):
         test = FAST.fast_test_preliminary([100, 50, 150, 50, 150], 25)
         self.assertEqual(False, test)  # add assertion here
 
+    def test_continous_9(self):
+        test = FAST.fast_check_continous_9(100, [150, 150, 150, 150,
+                                                 150, 150, 150, 150,
+                                                 150, 100, 100, 100,
+                                                 100, 100, 100, 100, ], 25)
+        self.assertTrue(test, "there was one, found it")
+        test2 = FAST.fast_check_continous_9(100, [150, 150, 150, 150,
+                                                 150, 100, 150, 150,
+                                                 150, 100, 100, 100,
+                                                 100, 100, 100, 100, ], 25)
+        self.assertFalse(test2, "There wasn't one, didn't found one")
+
 
 if __name__ == '__main__':
     unittest.main()
