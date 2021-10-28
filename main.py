b3d80579430cb1c91a2ed3986965a50d9d783e05
Tristan <tristan@5schwoerer.de>
2021-10-28 16:42:40 +0200

    implemented call to brief function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from FAST import *
 from brief import *
 from offset_vector import *
@@ -9,6 +10,7 @@ main file skeleton that probably does not work
 # TODO add output previews if not added in functions themselves already
 
 image_location = "./test.jpg"
+image_location=r'/Users/tristan/OneDrive - Aalborg Universitet/Aalborg University/Semester1/Perception/Exercises/aau-city-1.jpg'
 
 pic = cv2.imread(image_location)
 monochrome = cv.cvtColor(pic, cv2.COLOR_BGR2GRAY)
@@ -16,23 +18,26 @@ monochrome = cv.cvtColor(pic, cv2.COLOR_BGR2GRAY)
 fast_detector = Detector()
 patches = fast_detector.end_to_end(monochrome)
 
-# TODO check if logic is correct
-harris_measures = np.ndarray()
-for patch in patches:
-    patch = np.float32(patch)
-    patch_harris_values = cv2.cornerHarris(patch, 2, 3, 0.04) # TODO check params
-    np.append(harris_measures, np.sum(patch_harris_values))
+brief(image_location,patches,31)
 
-N_patches = 100
-ind = np.argpartition(harris_measures, -N_patches)[-N_patches:]
 
-patches = patches[ind]
-
-descriptors = np.ndarray()
-for patch in patches:
-    C, theta = offset_vector(patch)
-    patch_descriptors = brief(patch, theta)
-    descriptors = np.vstack([descriptors, patch_descriptors])
+# # TODO check if logic is correct
+# harris_measures = np.ndarray()
+# for patch in patches:
+#     patch = np.float32(patch)
+#     patch_harris_values = cv2.cornerHarris(patch, 2, 3, 0.04) # TODO check params
+#     np.append(harris_measures, np.sum(patch_harris_values))
+#
+# N_patches = 100
+# ind = np.argpartition(harris_measures, -N_patches)[-N_patches:]
+#
+# patches = patches[ind]
+#
+# descriptors = np.ndarray()
+# for patch in patches:
+#     C, theta = offset_vector(patch)
+#     patch_descriptors = brief(patch, theta)
+#     descriptors = np.vstack([descriptors, patch_descriptors])
 
 
 
