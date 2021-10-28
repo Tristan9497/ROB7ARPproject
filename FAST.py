cd70dab5029b299847950517424c64d54c78d08c
TheDefterus <TheDefterus@gmail.com>
2021-10-28 11:23:53 +0200

    commit of base FAST portions, WIP

--- FAST.py
+++ FAST.py
@@ -8,86 +8,93 @@ import argparse
 import os
 
 
-# Takes as input a central pixel value, cardinal pixel values, threshold and outputs the judgement as a boolean
-def fast_test_preliminary(pixels, threshold):
-    it_is_brighter = True
-    it_is_darker = True
-    counter_b = 0
-    counter_d = 0
-    for Intensity in range(len(pixels[1:])):
-        if pixels[0] + threshold >= pixels[Intensity + 1]:
-            counter_b += 1
-            if counter_b == 2:
-                it_is_brighter = False
-        if pixels[0] - threshold <= pixels[Intensity + 1]:
-            counter_d += 1
-            if counter_d == 2:
-                it_is_darker = False
-    return it_is_brighter or it_is_darker
+class Detector:
+    def __init__(self, n=9, threshold=25):
+        if not 8 < n < 16:
+            print(n, " is not a valid parameter, expect errors")
+        self.n = n
+        self.threshold = threshold
+        self.bright_array_n = []
+        self.dark_array_n = []
+        for i in range(n):
+            self.bright_array_n += 'b'
+            self.dark_array_n += 'd'
 
+    # Takes as input a central pixel value, cardinal pixel values, threshold and outputs the judgement as a boolean
+    def test_preliminary(self, pixels):
+        it_is_brighter = True
+        it_is_darker = True
+        counter_b = 0
+        counter_d = 0
+        for Intensity in range(len(pixels[1:])):
+            if pixels[0] + self.threshold >= pixels[Intensity + 1]:
+                counter_b += 1
+                if counter_b == 2:
+                    it_is_brighter = False
+            if pixels[0] - self.threshold <= pixels[Intensity + 1]:
+                counter_d += 1
+                if counter_d == 2:
+                    it_is_darker = False
+        return it_is_brighter or it_is_darker
 
-# apply the quicktest to every pixel in the input image with threshold, output candidates
-def fast_check_every_preliminary(monoc_image, threshold):
-    candidates = []
-    for i in range(monoc_image.shape[0] - 6):
-        for j in range(monoc_image.shape[1] - 6):
-            pixels = [monoc_image[i + 3, j + 3],
-                      monoc_image[i + 3, j + 6],
-                      monoc_image[i + 6, j + 3],
-                      monoc_image[i, j + 3],
-                      monoc_image[i + 3, j]]
-            if fast_test_preliminary(pixels, threshold):
-                candidates.append([j, i])
-    return candidates
+    # apply the quicktest to every pixel in the input image with threshold, output candidates
+    def check_every_preliminary(self, monoc_image):
+        candidates = []
+        for i in range(monoc_image.shape[0] - 6):
+            for j in range(monoc_image.shape[1] - 6):
+                pixels = [monoc_image[i + 3, j + 3],
+                          monoc_image[i + 3, j + 6],
+                          monoc_image[i + 6, j + 3],
+                          monoc_image[i, j + 3],
+                          monoc_image[i + 3, j]]
+                if Detector.test_preliminary(self, pixels):
+                    candidates.append([j, i])
+        return candidates
 
+    def get_conti_pixels(self, monoc_image, candidate):
+        values = [monoc_image[candidate[0] + 3, candidate[1]],
+                  monoc_image[candidate[0] + 4, candidate[1]],
+                  monoc_image[candidate[0] + 5, candidate[1]] + 1,
+                  monoc_image[candidate[0] + 6, candidate[1]] + 2,
+                  monoc_image[candidate[0] + 6, candidate[1]] + 3,
+                  monoc_image[candidate[0] + 6, candidate[1]] + 4,
+                  monoc_image[candidate[0] + 5, candidate[1]] + 5,
+                  monoc_image[candidate[0] + 4, candidate[1]] + 6,
+                  monoc_image[candidate[0] + 3, candidate[1]] + 6,
+                  monoc_image[candidate[0] + 2, candidate[1]] + 6,
+                  monoc_image[candidate[0] + 1, candidate[1]] + 5,
+                  monoc_image[candidate[0], candidate[1]] + 4,
+                  monoc_image[candidate[0], candidate[1]] + 3,
+                  monoc_image[candidate[0], candidate[1]] + 2,
+                  monoc_image[candidate[0] + 1, candidate[1]] + 1,
+                  monoc_image[candidate[0] + 2, candidate[1]],
+                  ]
+        return values
 
-def fast_get_conti_pixels(monoc_image, candidate):
-    values = [monoc_image[candidate[0] + 3, candidate[1]],
-              monoc_image[candidate[0] + 4, candidate[1]],
-              monoc_image[candidate[0] + 5, candidate[1]] + 1,
-              monoc_image[candidate[0] + 6, candidate[1]] + 2,
-              monoc_image[candidate[0] + 6, candidate[1]] + 3,
-              monoc_image[candidate[0] + 6, candidate[1]] + 4,
-              monoc_image[candidate[0] + 5, candidate[1]] + 5,
-              monoc_image[candidate[0] + 4, candidate[1]] + 6,
-              monoc_image[candidate[0] + 3, candidate[1]] + 6,
-              monoc_image[candidate[0] + 2, candidate[1]] + 6,
-              monoc_image[candidate[0] + 1, candidate[1]] + 5,
-              monoc_image[candidate[0], candidate[1]] + 4,
-              monoc_image[candidate[0], candidate[1]] + 3,
-              monoc_image[candidate[0], candidate[1]] + 2,
-              monoc_image[candidate[0] + 1, candidate[1]] + 1,
-              monoc_image[candidate[0] + 2, candidate[1]],
-              ]
-    return values
-
-
-# takes as input the pixel values of the circle (+ center), threshold and outputs boolean regarding candidacy
-def fast_check_continous_9(center_value, pixel_values, threshold):
-    list_of_pixel_values = pixel_values
-    bright_or_dark = []
-    judgement = False
-    for i in range(len(list_of_pixel_values)):
-        temp_value = list_of_pixel_values[i]
-        if temp_value >= center_value + threshold:
-            bright_or_dark += 'b'
-        elif temp_value <= center_value - threshold:
-            bright_or_dark += 'd'
-        else:
-            bright_or_dark += 's'
-    for i in range(len(list_of_pixel_values)):
-        for bds in bright_or_dark:
-            if list_of_pixel_values[0:9] == 'b' or list_of_pixel_values[0:9] == 'b':
+    # takes as input the pixel values of the circle (+ center), threshold and outputs boolean regarding candidacy
+    def check_continous(self, center_value, pixel_values):
+        bright_or_dark = []
+        judgement = False
+        for i in range(len(pixel_values)):
+            temp_value = pixel_values[i]
+            if temp_value >= center_value + self.threshold:
+                bright_or_dark += 'b'
+            elif temp_value <= center_value - self.threshold:
+                bright_or_dark += 'd'
+            else:
+                bright_or_dark += 's'
+        for i in range(len(pixel_values)):
+            # print(bright_or_dark, judgement)
+            if bright_or_dark[0:self.n] == self.bright_array_n or bright_or_dark[0:self.n] == self.dark_array_n:
                 judgement = True
                 break
             bright_or_dark.append(bright_or_dark.pop(0))
-    return judgement
+        return judgement
 
-
-def fast_end_to_end(monoc_image, threshold):
-    cadidate_locations = fast_check_every_preliminary(monoc_image, threshold)
-    features_detected_end = []
-    return features_detected_end
+    def end_to_end(self, monoc_image):
+        cadidate_locations = Detector.check_every_preliminary(self, monoc_image)
+        features_detected_end = []
+        return features_detected_end
 
 
 # pic = cv.imread('/home/thekinga/University/Polybot/TestMap_cropped.jpg')
--- FAST_test.py
+++ FAST_test.py
@@ -1,27 +1,37 @@
 import unittest
 import FAST
 
+n = 11
+test_detector = FAST.Detector(n)
+
 
 class MyTestCase(unittest.TestCase):
     def test_does_it_work_at_all(self):
-        test = FAST.fast_test_preliminary([100, 50, 50, 50, 50], 25)
+        test = test_detector.test_preliminary([100, 50, 50, 50, 50])
         self.assertEqual(True, test)  # add assertion here
 
     def test_different_parity(self):
-        test = FAST.fast_test_preliminary([100, 50, 150, 50, 150], 25)
+        test = test_detector.test_preliminary([100, 50, 150, 50, 150])
         self.assertEqual(False, test)  # add assertion here
 
-    def test_continous_9(self):
-        test = FAST.fast_check_continous_9(100, [150, 150, 150, 150,
+    def test_continous(self):
+        test = test_detector.check_continous(100, [150, 150, 150, 150,
                                                  150, 150, 150, 150,
-                                                 150, 100, 100, 100,
-                                                 100, 100, 100, 100, ], 25)
-        self.assertTrue(test, "there was one, found it")
-        test2 = FAST.fast_check_continous_9(100, [150, 150, 150, 150,
+                                                 150, 150, 150, 100,
+                                                 100, 100, 100, 100, ])
+        self.assertTrue(test, "there was one, did not find it")
+        test2 = test_detector.check_continous(100, [150, 150, 150, 150,
                                                  150, 100, 150, 150,
                                                  150, 100, 100, 100,
-                                                 100, 100, 100, 100, ], 25)
-        self.assertFalse(test2, "There wasn't one, didn't found one")
+                                                 100, 100, 100, 100, ])
+        self.assertFalse(test2, "There wasn't one, but I found one??")
+
+    def test_continous_tricky(self):
+        test = test_detector.check_continous(100, [150, 150, 150, 150,
+                                                   150, 150, 150, 100,
+                                                   100, 100, 150, 100,
+                                                   150, 150, 150, 150, ])
+        self.assertTrue(test, "there was one, did not find it")
 
 
 if __name__ == '__main__':
